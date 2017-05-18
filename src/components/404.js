3e86aee84ae1ddfe8c2ce02c761dd0d9c7bc786c
chuyun <info_together@aliyun.com>
2017-05-18 16:21:56 +0800

    init

---
+++ src/components/404.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class NotFound extends React.Component {
+
+    render() {
+        return ( 
+            <div>
+                oh,404...
+           </div>
+        )
+    }
+}
+
+export default NotFound;
